4111939d0688ffb9c00f00bfce506a33a40b2a63
Monica Chen <monicachen808@gmail.com>
2021-02-15 07:27:42 -0500

    updated bakery aisle page

--- bakery.php
+++ bakery.php
@@ -7,8 +7,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-    
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+
 
     <!-- Google font-->
     <link rel="preconnect" href="https://fonts.gstatic.com">
@@ -21,6 +21,16 @@
   </head>
   <style>
     /* Custom CSS */
+    
+img{
+    max-height: 250px;
+    max-width: 320px;
+}
+
+.aisle-item{
+    margin: 10px 0;
+}
+
 
 
   </style>
@@ -30,16 +40,255 @@
 
     <!--CONTENTS OF PAGE STARTS HERE-->
     <div class="custom-page-container">
-      <h1>BAKERY</h1>
+        <div style="margin-bottom: 20px">
+            <h1 style="font-size: 150%; text-align: center"> Bakery</h1>
+        </div>
 
+        <div class="d-flex flex-wrap justify-content-evenly"> 
+            
+            <div class="aisle-item"> 
+                <img src="resources/img/bakery-bagel2.jpg" alt="Bagel">
+                <h5 style=" font-family: 'Exo', sans-serif; font-weight: bold; margin-top: 10px;">Bagel</h5>
+                <p style="margin-bottom: 0 ">10$/dozen</p>
+                
+                <div class="d-flex bd-highlight">                    
+                    <div class="flex-grow-1 bd-highlight">
+                        <button class="btn btn-primary rounded-circle btn-sm py-0 " style="background-color:  #EE4F3E; border-color:  #EE4F3E;"> - </button>
+                        <span>1</span>
+                        <button class="btn btn-primary rounded-circle btn-sm py-0 " style="background-color:  #EE4F3E; border-color:  #EE4F3E;"> + </button>
+                    </div>
+                    
+                    <div class="bd-highlight">
+                        
+                    <button class="btn btn-primary rounded-3 btn-sm btn-sm py-0 " style="background-color:  #EE4F3E; border-color: #EE4F3E;"> Add to Cart</button>
+                    </div>
+                </div>
+            </div>
+            
+            
+            <div class="aisle-item"> 
+                <img src="resources/img/bakery-baguette2.jpg" alt="Baguette">
+                 <h5 style=" font-family: 'Exo', sans-serif; font-weight: bold; margin-top: 10px;">Baguette</h5>
+                <p style="margin-bottom: 0 ">7$/unit</p>
+                
+                <div class="d-flex bd-highlight">                    
+                    <div class="flex-grow-1 bd-highlight">
+                        <button class="btn btn-primary rounded-circle btn-sm py-0 " style="background-color:  #EE4F3E; border-color:  #EE4F3E;"> - </button>
+                        <span>1</span>
+                        <button class="btn btn-primary rounded-circle btn-sm py-0 " style="background-color:  #EE4F3E; border-color:  #EE4F3E;"> + </button>
+                    </div>
+                    
+                    <div class="bd-highlight">
+                        
+                    <button class="btn btn-primary rounded-3 btn-sm btn-sm py-0 " style="background-color:  #EE4F3E; border-color: #EE4F3E;"> Add to Cart</button>
+                    </div>
+                </div>
+            </div>
+            
+            
+            <div class="aisle-item"> 
+                <img src="resources/img/bakery-challah2.jpg" alt="Challah">
+                <h5 style=" font-family: 'Exo', sans-serif; font-weight: bold; margin-top: 10px;">Challah</h5>
+                <p style="margin-bottom: 0 ">5$/unit</p>
+                
+                <div class="d-flex bd-highlight">                    
+                    <div class="flex-grow-1 bd-highlight">
+                        <button class="btn btn-primary rounded-circle btn-sm py-0 " style="background-color:  #EE4F3E; border-color:  #EE4F3E;"> - </button>
+                        <span>1</span>
+                        <button class="btn btn-primary rounded-circle btn-sm py-0 " style="background-color:  #EE4F3E; border-color:  #EE4F3E;"> + </button>
+                    </div>
+                    
+                    <div class="bd-highlight">
+                        
+                    <button class="btn btn-primary rounded-3 btn-sm btn-sm py-0 " style="background-color:  #EE4F3E; border-color: #EE4F3E;"> Add to Cart</button>
+                    </div>
+                </div>
+            </div>
+            
+            
+            <div class="aisle-item"> 
+                <img src="resources/img/bakery-chocolatine2.jpg" alt="Chocolatine"> 
+                <h5 style=" font-family: 'Exo', sans-serif; font-weight: bold; margin-top: 10px;">Chocolatine</h5>
+                <p style="margin-bottom: 0 ">3$/unit</p>
+                
+                <div class="d-flex bd-highlight">                    
+                    <div class="flex-grow-1 bd-highlight">
+                        <button class="btn btn-primary rounded-circle btn-sm py-0 " style="background-color:  #EE4F3E; border-color:  #EE4F3E;"> - </button>
+                        <span>1</span>
+                        <button class="btn btn-primary rounded-circle btn-sm py-0 " style="background-color:  #EE4F3E; border-color:  #EE4F3E;"> + </button>
+                    </div>
+                    
+                    <div class="bd-highlight">
+                        
+                    <button class="btn btn-primary rounded-3 btn-sm btn-sm py-0 " style="background-color:  #EE4F3E; border-color: #EE4F3E;"> Add to Cart</button>
+                    </div>
+                </div>
+            </div>
+            
+            <div class="aisle-item">
+                <img src="resources/img/bakery-ciabatta2.jpg" alt="Ciabatta">
+                <h5 style=" font-family: 'Exo', sans-serif; font-weight: bold; margin-top: 10px;">Ciabatta</h5>
+                <p style="margin-bottom: 0 ">7$/unit</p>
+                
+                <div class="d-flex bd-highlight">                    
+                    <div class="flex-grow-1 bd-highlight">
+                        <button class="btn btn-primary rounded-circle btn-sm py-0 " style="background-color:  #EE4F3E; border-color:  #EE4F3E;"> - </button>
+                        <span>1</span>
+                        <button class="btn btn-primary rounded-circle btn-sm py-0 " style="background-color:  #EE4F3E; border-color:  #EE4F3E;"> + </button>
+                    </div>
+                    
+                    <div class="bd-highlight">
+                        
+                    <button class="btn btn-primary rounded-3 btn-sm btn-sm py-0 " style="background-color:  #EE4F3E; border-color: #EE4F3E;"> Add to Cart</button>
+                    </div>
+                </div>
+            </div>
+            
+            <div class="aisle-item"> 
+                <img src="resources/img/bakery-cinnamon2.jpg" alt="Cinammon Roll">
+                <h5 style=" font-family: 'Exo', sans-serif; font-weight: bold; margin-top: 10px;">Cinnamon Roll</h5>
+                <p style="margin-bottom: 0 ">10$/pack (6)</p>
+                
+                <div class="d-flex bd-highlight">                    
+                    <div class="flex-grow-1 bd-highlight">
+                        <button class="btn btn-primary rounded-circle btn-sm py-0 " style="background-color:  #EE4F3E; border-color:  #EE4F3E;"> - </button>
+                        <span>1</span>
+                        <button class="btn btn-primary rounded-circle btn-sm py-0 " style="background-color:  #EE4F3E; border-color:  #EE4F3E;"> + </button>
+                    </div>
+                    
+                    <div class="bd-highlight">
+                        
+                    <button class="btn btn-primary rounded-3 btn-sm btn-sm py-0 " style="background-color:  #EE4F3E; border-color: #EE4F3E;"> Add to Cart</button>
+                    </div>
+                </div>
+            </div>
+            
+            <div class="aisle-item"> 
+                <img src="resources/img/bakery-croissant2.jpg" alt="Croissant">
+                <h5 style=" font-family: 'Exo', sans-serif; font-weight: bold; margin-top: 10px;">Croissant</h5>
+                <p style="margin-bottom: 0 ">11$/pack (6)</p>
+                
+                <div class="d-flex bd-highlight">                    
+                    <div class="flex-grow-1 bd-highlight">
+                        <button class="btn btn-primary rounded-circle btn-sm py-0 " style="background-color:  #EE4F3E; border-color:  #EE4F3E;"> - </button>
+                        <span>1</span>
+                        <button class="btn btn-primary rounded-circle btn-sm py-0 " style="background-color:  #EE4F3E; border-color:  #EE4F3E;"> + </button>
+                    </div>
+                    
+                    <div class="bd-highlight">
+                        
+                    <button class="btn btn-primary rounded-3 btn-sm btn-sm py-0 " style="background-color:  #EE4F3E; border-color: #EE4F3E;"> Add to Cart</button>
+                    </div>
+                </div>
+            </div>
+            
+            <div class="aisle-item"> 
+                <img src="resources/img/bakery-donut2.jpg" alt="Donut">
+                <h5 style=" font-family: 'Exo', sans-serif; font-weight: bold; margin-top: 10px;">Donut</h5>
+                <p style="margin-bottom: 0 ">10$/dozen</p>
+                
+                <div class="d-flex bd-highlight">                    
+                    <div class="flex-grow-1 bd-highlight">
+                        <button class="btn btn-primary rounded-circle btn-sm py-0 " style="background-color:  #EE4F3E; border-color:  #EE4F3E;"> - </button>
+                        <span>1</span>
+                        <button class="btn btn-primary rounded-circle btn-sm py-0 " style="background-color:  #EE4F3E; border-color:  #EE4F3E;"> + </button>
+                    </div>
+                    
+                    <div class="bd-highlight">
+                        
+                    <button class="btn btn-primary rounded-3 btn-sm btn-sm py-0 " style="background-color:  #EE4F3E; border-color: #EE4F3E;"> Add to Cart</button>
+                    </div>
+                </div>
+            </div>
+             
+            <div class="aisle-item">
+                <img src="resources/img/bakery-pita2.jpg" alt="Pita">
+                <h5 style=" font-family: 'Exo', sans-serif; font-weight: bold; margin-top: 10px;">Pita</h5>
+                <p style="margin-bottom: 0 ">5$/bag (8)</p>
+                
+                <div class="d-flex bd-highlight">                    
+                    <div class="flex-grow-1 bd-highlight">
+                        <button class="btn btn-primary rounded-circle btn-sm py-0 " style="background-color:  #EE4F3E; border-color:  #EE4F3E;"> - </button>
+                        <span>1</span>
+                        <button class="btn btn-primary rounded-circle btn-sm py-0 " style="background-color:  #EE4F3E; border-color:  #EE4F3E;"> + </button>
+                    </div>
+                    
+                    <div class="bd-highlight">
+                        
+                    <button class="btn btn-primary rounded-3 btn-sm btn-sm py-0 " style="background-color:  #EE4F3E; border-color: #EE4F3E;"> Add to Cart</button>
+                    </div>
+                </div>
+            </div>
+            
+            <div class="aisle-item"> 
+                <img src="resources/img/bakery-pretzel2.jpg" alt="Pretzel">
+                <h5 style=" font-family: 'Exo', sans-serif; font-weight: bold; margin-top: 10px;">Pretzel</h5>
+                <p style="margin-bottom: 0 ">4$/unit</p>
+                
+                <div class="d-flex bd-highlight">                    
+                    <div class="flex-grow-1 bd-highlight">
+                        <button class="btn btn-primary rounded-circle btn-sm py-0 " style="background-color:  #EE4F3E; border-color:  #EE4F3E;"> - </button>
+                        <span>1</span>
+                        <button class="btn btn-primary rounded-circle btn-sm py-0 " style="background-color:  #EE4F3E; border-color:  #EE4F3E;"> + </button>
+                    </div>
+                    
+                    <div class="bd-highlight">
+                        
+                    <button class="btn btn-primary rounded-3 btn-sm btn-sm py-0 " style="background-color:  #EE4F3E; border-color: #EE4F3E;"> Add to Cart</button>
+                    </div>
+                </div>
+            </div>
+            
+            <div class="aisle-item"> 
+                <img src="resources/img/bakery-wheat-bread2.jpg" alt="Wheat Bread">
+                <h5 style=" font-family: 'Exo', sans-serif; font-weight: bold; margin-top: 10px;">Wheat Bread</h5>
+                <p style="margin-bottom: 0 ">7$/unit</p>
+                
+                <div class="d-flex bd-highlight">                    
+                    <div class="flex-grow-1 bd-highlight">
+                        <button class="btn btn-primary rounded-circle btn-sm py-0 " style="background-color:  #EE4F3E; border-color:  #EE4F3E;"> - </button>
+                        <span>1</span>
+                        <button class="btn btn-primary rounded-circle btn-sm py-0 " style="background-color:  #EE4F3E; border-color:  #EE4F3E;"> + </button>
+                    </div>
+                    
+                    <div class="bd-highlight">
+                        
+                    <button class="btn btn-primary rounded-3 btn-sm btn-sm py-0 " style="background-color:  #EE4F3E; border-color: #EE4F3E;"> Add to Cart</button>
+                    </div>
+                </div>
+            </div>
+            
+            <div class="aisle-item"> 
+                <img src="resources/img/bakery-white-bread2.jpg" alt="White Bread"> 
+                <h5 style=" font-family: 'Exo', sans-serif; font-weight: bold; margin-top: 10px;">White Bread</h5>
+                <p style="margin-bottom: 0 ">7$/bag</p>
+                
+                <div class="d-flex bd-highlight">                    
+                    <div class="flex-grow-1 bd-highlight">
+                        <button class="btn btn-primary rounded-circle btn-sm py-0 " style="background-color:  #EE4F3E; border-color:  #EE4F3E;"> - </button>
+                        <span>1</span>
+                        <button class="btn btn-primary rounded-circle btn-sm py-0 " style="background-color:  #EE4F3E; border-color:  #EE4F3E;"> + </button>
+                    </div>
+                    
+                    <div class="bd-highlight">
+                        
+                    <button class="btn btn-primary rounded-3 btn-sm btn-sm py-0 " style="background-color:  #EE4F3E; border-color: #EE4F3E;"> Add to Cart</button>
+                    </div>
+                </div>
+            </div>    
+            
+        </div>
     </div> 
 
 
     <?php include "includes/footer.html"; ?>
 
     <!-- Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.6.0/dist/umd/popper.min.js" integrity="sha384-KsvD1yqQ1/1+IA7gi3P0tyJcT3vR+NdBTt13hSJ2lnve8agRGXTTyNaBYmCR/Nwi" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.min.js" integrity="sha384-nsg8ua9HAw1y0W1btsyWgBklPnCUAFLuTMS2G72MMONqmOymq585AcH49TLBQObG" crossorigin="anonymous"></script>
+
+    
+    
     <script>
         // Custom JS
     </script>
