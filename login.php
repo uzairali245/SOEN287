43e81a51d8cddb042e6c975128596236ca0d1f51
David Lemme <daviddantelemme@gmail.com>
2021-02-17 15:48:02 -0500

    finalaized the logina dn signup page they are responsive to changes in size and header works

--- login.php
+++ login.php
@@ -2,7 +2,7 @@
 <html lang="en">
 
 <Head>
-    <title>User login</title>
+    <title>Login</title>
 
     <meta charset="utf-8">
 
@@ -19,78 +19,74 @@
     <!--link to stylesheet -->
     <link rel="stylesheet" href="resources\css\style.css">
     <style>
-        .login-container {
-            width: 370px;
-            height: 400px;
-            top: 50%;
-            left: 50%;
-            position: absolute;
+        .container {
+            width: 375px;
+            margin-top: 15%;
             border-style: solid;
+            padding: 20px;
             border-width: 2px;
-            transform: translate(-50%, -50%);
-            box-sizing: border-box;
-            color: #fff;
-            padding: 40px 20px;
         }
 
-
-
-        .login-container h1 {
+        .container h1 {
             text-align: center;
-            font-size: 20px;
-            margin-bottom: 25px;
+            font-size: 40px;
+            margin-bottom: 40px;
         }
 
-        .login-container label {
-            display: block;
-        }
-
-        .login-container div {
-            margin-bottom: 20px;
-        }
-
-        .login-container input[type="text"],
-        input[type="password"] {
-            width: 100%;
+        .container input[type="text"],
+        input[type="password"],
+        input[type="email"] {
+            width: 220%;
             border: 1px solid;
             background: transparent;
             color: white;
             height: 40px;
+            border-radius: 0px;
         }
 
-        .login-container input[type="text"]::placeholder,
-        input[type="password"]::placeholder {
+        .container input[type="text"]::placeholder,
+        input[type="password"]::placeholder,
+        input[type="email"]::placeholder {
             text-align: center;
             color: #d3d3d3;
 
         }
 
-        .login-container input[type="submit"] {
+        .container input[type="submit"] {
+            margin-top: 20px;
             width: 100%;
             border: none;
             outline: none;
             height: 40px;
             background-color: #EE4F3E;
             color: #ffff;
-
+            border-radius: 0px;
         }
 
-        .login-container input[type="submit"]:hover {
+        .container input[type="submit"]:hover {
             cursor: pointer;
             box-shadow: 1px 1px 10px #696969;
         }
 
-        a {
+        .container a:hover {
+            color: #d3d3d3;
+
+        }
+
+        .container a {
+
             display: block;
             text-align: left;
             text-decoration: none;
             color: white;
             font-size: 12px;
             margin-top: 10px;
+
         }
 
-        a:hover {
-            color: #d3d3d3;
+        .container input:focus {
+
+            background-color: #99442B;
 
         }
     </style>
@@ -103,31 +99,35 @@
 <body>
     <?php include "includes/header.html"; ?>
 
-    <div class="login-container">
-        <form action="">
+    <body>
+
+        <div class="container">
             <h1>LOGIN</h1>
-            <div>
-                <label>Username</label>
-                <input type="text" name="username" placeholder="Enter your username here" value="">
-                
+            <form>
+                <div class="row jumbotron">
 
-            </div>
-            <div>
-                <label>Password</label>
-                <input type="password" name="password" placeholder="Enter your password here" value="">
-            </div>
-            <input type="submit" name="login" value="LOGIN">
-            <a href="#">Forgot Password?</a>
-            <a href="signup.php">Not a Member?</a>
-        </form>
-    </div>
+                    <div class="col-sm-6 form-group">
+                        <label for="pass">Email</label>
+                        <input type="email" name="password" class="form-control" id="pass" placeholder="Enter your Email." required>
+                        <label for="pass"> Password</label>
+                        <input type="Password" name="cnf-password" class="form-control" id="pass2" placeholder="Re-enter your password." required>
+                    </div>
 
-    <?php include "includes/footer.html"; ?>
-</body>
+                    <div class="col-sm-12 form-group mb-0">
+                        <input type="submit" name="signup" value="SIGNUP">
+                    </div>
+                    <a href="signup.php">Already a Member? Sign in!</a>
 
-<!-- Bootstrap JS -->
-<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.6.0/dist/umd/popper.min.js" integrity="sha384-KsvD1yqQ1/1+IA7gi3P0tyJcT3vR+NdBTt13hSJ2lnve8agRGXTTyNaBYmCR/Nwi" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.min.js" integrity="sha384-nsg8ua9HAw1y0W1btsyWgBklPnCUAFLuTMS2G72MMONqmOymq585AcH49TLBQObG" crossorigin="anonymous"></script>
+                </div>
+            </form>
+        </div>
+
+        <?php include "includes/footer.html"; ?>
+    </body>
+
+    <!-- Bootstrap JS -->
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.6.0/dist/umd/popper.min.js" integrity="sha384-KsvD1yqQ1/1+IA7gi3P0tyJcT3vR+NdBTt13hSJ2lnve8agRGXTTyNaBYmCR/Nwi" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.min.js" integrity="sha384-nsg8ua9HAw1y0W1btsyWgBklPnCUAFLuTMS2G72MMONqmOymq585AcH49TLBQObG" crossorigin="anonymous"></script>
 
 
 </html>
--- signup.php
+++ signup.php
@@ -2,7 +2,7 @@
 <html lang="en">
 
 <Head>
-    <title>Usignup</title>
+    <title>Signup</title>
 
     <meta charset="utf-8">
 
@@ -19,79 +19,74 @@
     <!--link to stylesheet -->
     <link rel="stylesheet" href="resources\css\style.css">
     <style>
-        .signup-container {
+        .container {
             width: 600px;
-            height: 400px;
-            top: 50%;
-            left: 50%;
-            position: absolute;
+            margin-top: 10%;
             border-style: solid;
+            padding: 20px;
             border-width: 2px;
-            transform: translate(-50%, -50%);
-            box-sizing: border-box;
-            color: #fff;
-            padding: 40px 20px;
         }
 
-
-
-        .signup-container h1 {
+        .container h1 {
             text-align: center;
-            font-size: 20px;
-            margin-bottom: 25px;
+            font-size: 40px;
+            margin-bottom: 40px;
         }
 
-        .signup-container label {
-            display: block;
-        }
-
-        .signup-container div {
-            margin-bottom: 20px;
-        }
-
-        .signup-container input[type="text"],
-        input[type="password"] {
-            width: 45%;
+        .container input[type="text"],
+        input[type="password"],
+        input[type="email"] {
+            width: 100%;
             border: 1px solid;
             background: transparent;
             color: white;
             height: 40px;
+            border-radius: 0px;
         }
 
-        .signup-container input[type="text"]::placeholder,
-        input[type="password"]::placeholder {
+        .container input[type="text"]::placeholder,
+        input[type="password"]::placeholder,
+        input[type="email"]::placeholder {
             text-align: center;
             color: #d3d3d3;
 
         }
 
-        .signup-container input[type="submit"] {
-            width: 50%;
+        .container input[type="submit"] {
+            margin-top: 20px;
+            width: 100%;
             border: none;
             outline: none;
             height: 40px;
             background-color: #EE4F3E;
             color: #ffff;
-            margin-left: 25%;
-
+            border-radius: 0px;
         }
 
-        .signup-container input[type="submit"]:hover {
+        .container input[type="submit"]:hover {
             cursor: pointer;
             box-shadow: 1px 1px 10px #696969;
         }
 
-        a {
+        .container a:hover {
+            color: #d3d3d3;
+
+        }
+
+        .container input:focus {
+
+            background-color: #99442B;
+
+        }
+
+        .container a {
+
             display: block;
-            text-align: center;
+            text-align: left;
             text-decoration: none;
             color: white;
             font-size: 12px;
             margin-top: 10px;
-        }
-
-        a:hover {
-            color: #d3d3d3;
 
         }
     </style>
@@ -104,31 +99,59 @@
 <body>
     <?php include "includes/header.html"; ?>
 
-    <div class="signup-container">
-        <form action="">
-            <h1>SIGNUP</h1>
-            <div>
-                <label>Username</label>
-                <input class="signup-input" type="text" name="username" placeholder="Enter your username here" value="">
+    <body>
 
+        <div class="container">
+            <h1>Signup</h1>
+            <form>
+                <div class="row jumbotron">
+                    <div class="col-sm-6 form-group">
+                        <label for="name-f">First Name</label>
+                        <input type="text" class="form-control" name="fname" placeholder="Enter your first name." required>
+                    </div>
+                    <div class="col-sm-6 form-group">
+                        <label for="name-l">Last name</label>
+                        <input type="text" class="form-control" name="lname" placeholder="Enter your last name." required>
+                    </div>
+                    <div class="col-sm-6 form-group">
+                        <label for="email">Email</label>
+                        <input type="email" class="form-control" name="email" id="email" placeholder="Enter your email." required>
+                    </div>
+                    <div class="col-sm-6 form-group">
+                        <label for="name-l">Confirm Email</label>
+                        <input type="email" class="form-control" name="lname" placeholder="Confirm Email." required>
+                    </div>
+                    <div class="col-sm-6 form-group">
+                        <label for="name-l">Language</label>
+                        <input type="text" class="form-control" name="lname" placeholder="Enter your Language." required>
+                    </div>
+                    <div class="col-sm-6 form-group">
+                        <label for="province">Province</label>
+                        <input type="text" class="form-control" name="State" id="State" placeholder="Enter your Province." required>
+                    </div>
+                    <div class="col-sm-6 form-group">
+                        <label for="pass">Password</label>
+                        <input type="Password" name="password" class="form-control" id="pass" placeholder="Enter your password." required>
+                    </div>
+                    <div class="col-sm-6 form-group">
+                        <label for="pass2">Confirm Password</label>
+                        <input type="Password" name="cnf-password" class="form-control" id="pass2" placeholder="Re-enter your password." required>
+                    </div>
+                    <div class="col-sm-12 form-group mb-0">
+                        <input type="submit" name="signup" value="SIGNUP">
+                    </div>
+                    <a href="login.php">Already a Member? Sign in!</a>
 
-            </div>
-            <div>
-                <label>Password</label>
-                <input class="signup-input" type="password" name="password" placeholder="Enter your password here" value="">
-            </div>
-            <input type="submit" name="signup" value="signup">
-            <a href="#">Forgot Password?</a>
-            <a href="signup.php">Not a Member?</a>
-        </form>
-    </div>
+                </div>
+            </form>
+        </div>
 
-    <?php include "includes/footer.html"; ?>
-</body>
+        <?php include "includes/footer.html"; ?>
+    </body>
 
-<!-- Bootstrap JS -->
-<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.6.0/dist/umd/popper.min.js" integrity="sha384-KsvD1yqQ1/1+IA7gi3P0tyJcT3vR+NdBTt13hSJ2lnve8agRGXTTyNaBYmCR/Nwi" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.min.js" integrity="sha384-nsg8ua9HAw1y0W1btsyWgBklPnCUAFLuTMS2G72MMONqmOymq585AcH49TLBQObG" crossorigin="anonymous"></script>
+    <!-- Bootstrap JS -->
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.6.0/dist/umd/popper.min.js" integrity="sha384-KsvD1yqQ1/1+IA7gi3P0tyJcT3vR+NdBTt13hSJ2lnve8agRGXTTyNaBYmCR/Nwi" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.min.js" integrity="sha384-nsg8ua9HAw1y0W1btsyWgBklPnCUAFLuTMS2G72MMONqmOymq585AcH49TLBQObG" crossorigin="anonymous"></script>
 
 
 </html>
