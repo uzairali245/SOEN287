84f62b2c247677ea2da1d54922a9cf6b5f0c9ee3
David Lemme <daviddantelemme@gmail.com>
2021-02-16 22:52:28 -0500

    made minor changes to login page finished signup.php and moved signup.php css to stylesheet

--- login.php
+++ login.php
@@ -14,11 +14,13 @@
     <div class="login-page">
         <div class="form">
             <h3 class="form-login-header">Login</h3>
+            <br><br>
             <form class="register-form">
-                <label for="email">Email</label>
-                <input class="input-login" placeholder="Enter Email Here " id="email" type="text" />
-                <label for="password">Password</label>
-                <input class="input-login" placeholder="Enter Password Here" id="password" type="text" />
+                <!-- <label for="email">Email</label> -->
+                <input class="input-login" placeholder="Email" id="email" type="text" />
+                <!-- <label for="password">Password</label> -->
+                <br></br>
+                <input class="input-login" placeholder="Password" id="password" type="text" />
                 <button class="login">Login</button><br><br>
                 <label class="sub-login-label">Forgot Password? <a class="sub-login" href="#">Click Here</a></label><br><br>
                 <label class="sub-login-label">Not a Member? <a class="sub-login" href="signup.php">Click Here</a></label>
--- signup.php
+++ signup.php
@@ -4,112 +4,36 @@
 <Head>
     <title>Signup</title>
     <link rel="stylesheet" href="resources/css/style.css" type="text/css">
-    <style>
-        /*-------------------------------------------*/
-        /* signup Page*/
-        /*-------------------------------------------*/
-
-        
-
-        /*            pxiel size should only bge in pixels in the body */
-
-
-        .form {
-            display: block;
-            text-align: center;
-            margin-left: 600px;
-            max-width: 0px;
-            margin-top: 120px;
-            /*this is for the white border*/
-            border: 2px solid white;
-            box-sizing: border-box;
-            padding: 150px 555px 150px 150px;
-
-
-
-        }
-
-        button.signup {
-            display: inline-block;
-            margin-left: 0px;
-            margin-top: 10px;
-            padding: 3px 69px 3px 69px;
-            background-color: #EE4F3E;
-            border-radius: 0px;
-            color: aliceblue;
-            width: 400px
-        }
-
-
-
-        .register-form label {
-
-            font-size: 11px;
-            font-family: sans-serif;
-        }
-
-        .form-signup-header {
-            margin-left: 120px;
-            color: white;
-            font-size: 60px;
-
-        }
-
-        .signup:hover,
-        .signup:active {
-            background-color: #f16b5d;
-        }
-
-        .input-signup[type="text"] {
-            width: 400px;
-            height: 49px;
-            border: solid;
-            border-color: white;
-            border-width: 1px;
-        }
-
-        .input-signup::placeholder {
-            color: gainsboro;
-            text-align: left;
-
-        }
-
-        .sub-signup:hover {
-            color: black;
-        }
-
-        label.sub-signup-label {
-            margin-top: 1000px;
-            white-space: nowrap;
-            max-width: 0px;
-            font-size: 110%;
-        }
-
-        .register-form {
-            max-width: 0px;
-        }
-
-        .email,
-        .password {
-            font-size: 500%;
-        }
-    </style>
-
+    
 </Head>
 
 <body>
     <?php include "includes/header.html"; ?>
 
-    <div class="Signup-page">
-        <div class="form">
-            <h3 class="form-Signup-header">Signup</h3>
-            <form class="register-form">
-                <label for="email">Email</label>
-                <input class="input-Signup" placeholder="Enter Email Here " id="email" type="text" />
-                <label for="password">Password</label>
-                <input class="input-Signup" placeholder="Enter Password Here" id="password" type="text" />
-                <button class="Signup">Signup</button><br><br>
-                <label class="sub-Signup-label">Forgot Password? <a class="sub-Signup" href="#">Click Here</a></label>
+    <div class="signup-page">
+        <div class="form-signup-left">
+            <h3 class="form-signup-header">Signup</h3>
+            <br></br>
+            <form class="register-form-signup">
+
+                <input class="input-signup" placeholder="Email " id="email" type="text" />
+
+                <input class="input-signup" placeholder="Confirm Email" id="confirm-email" type="text" />
+                <br></br>
+                <input class="input-signup" placeholder="First name" id="fist-name" type="text" />
+
+                <input class="input-signup" placeholder="Last Name" id="last-name" type="text" />
+                <br></br>
+                <input class="input-signup" placeholder="Password" id="password" type="text" />
+
+                <input class="input-signup" placeholder="Confirm Password" id="confirm-password" type="text" />
+                <br></br>
+                <input class="input-signup" placeholder="Language" id="language" type="text" />
+
+                <input class="input-signup" placeholder="Province" id="province" type="text" />
+                <br></br>
+                <button class="signup">Signup</button><br><br>
+                <label class="sub-signup-label">Already a Member? <a class="sub-signup" href="login.php">Click Here</a></label>
 
             </form>
         </div>
