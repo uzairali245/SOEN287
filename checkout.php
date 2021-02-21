4ea096e95aa7871a9d4a1a50c8ed4223d51a5bde
Anne <annebstn22@hotmail.ca>
2021-02-21 16:58:49 +0000

    changed title of checkout page and added author

--- checkout.php
+++ checkout.php
@@ -3,8 +3,9 @@
     
     <head>
         
-        <title>Product Page</title>
+        <title>Checkout</title>
         
+        <meta name="author" content="Anne Bastien">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta charset="utf-8">
         
