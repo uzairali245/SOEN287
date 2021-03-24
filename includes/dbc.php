9cdb6a6c8845975e1411111e24003d5564c1a3ce
Jay <roebuck.jayc@gmail.com>
2021-03-24 12:30:25 -0400

    added database connect file

---
+++ includes/dbc.php
@@ -0,0 +1,9 @@
+<?php
+    // Will have to change this information when connecting to webhost 
+    $dbServername = "localhost"; // You may have to change this
+    $dbUsername = "root";
+    $dbPassword = "root";
+    $dbName = "grocery-soen287"; 
+
+    $conn = mysqli_connect($dbServername,$dbUsername,$dbPassword,$dbName);
+?>
