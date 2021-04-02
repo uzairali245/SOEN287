765148993ceb5dfd9ef038f082f3aa192c3b7296
Anne <annebstn22@hotmail.ca>
2021-04-02 17:09:21 -0400

    added strawb instead of blueb

--- includes/session.php
+++ includes/session.php
@@ -2,7 +2,7 @@
     session_start();
 
     $_SESSION["customercart"] = array(
-        array(2,250,3), //Wild blueberries
+        array(2,250,3), //Strawberry
         array(8,12,4), // Roma Tomatoes
         array(6,null,4) // Pumpkin, null format - over inventory
     );
