a3bc26900cd4ca0ea9059bb645b81af31f1793db
Jay <roebuck.jayc@gmail.com>
2021-04-14 21:15:38 -0400

    put dbc back to normal D:<

--- includes/dbc.php
+++ includes/dbc.php
@@ -1,6 +1,6 @@
 <?php
     // Will have to change this information when connecting to webhost 
-    $dbServername = "localhost:8889"; // You may have to change this
+    $dbServername = "localhost"; // You may have to change this
     $dbUsername = "root";
     $dbPassword = "root";
     $dbName = "grocery-soen287"; 
